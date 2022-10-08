ade524cbb6fac3574d76fa1ef19aaa66959dd6b6
samlee <samuelwjlee@gmail.com>
2022-10-08 16:22:18 -0700

    Add minimal styling to goal comp

--- src/components/PublicGoals.tsx
+++ src/components/PublicGoals.tsx
@@ -4,19 +4,23 @@ import Loader from './Loader'
 
 export default function PublicGoals() {
   const [goals, isLoading] = useFetch(`${JSON_SERVER_URL}/goals`)
-
   return (
     <Loader isLoading={isLoading}>
-      <div>
-        <h1>Public Goals</h1>
-        {goals?.map((goal) => (
-          <>
-            <p>{goal?.title}</p>
+      <>
+        <h1 style={{ padding: 20 }}>Public Goals</h1>
+        {goals?.map((goal, idx) => (
+          <div
+            style={{
+              backgroundColor: idx % 2 === 0 ? 'white' : 'lightgray',
+              padding: 20,
+            }}
+          >
+            <h3>{goal?.title}</h3>
             <p>{goal?.description}</p>
-            <p>by {goal?.userId}</p>
-          </>
+            <p style={{ textAlign: 'right' }}>by {goal?.userId}</p>
+          </div>
         ))}
-      </div>
+      </>
     </Loader>
   )
 }
